f34743e85d888c4fcd1e197b8e2783d841f4f3d8
BK <brett.t.kizer@gmail.com>
2023-08-05 10:08:23 -0400

    Updates

---
+++ precache-manifest.63fb2749ceda6dbcda3a965da2a5d076.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1b1c90ec391b6dd4eb8f0c0b3595e5",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "600c044320c4f1ff644d",
+    "url": "/todolist/static/css/2.71303cd3.chunk.css"
+  },
+  {
+    "revision": "600c044320c4f1ff644d",
+    "url": "/todolist/static/js/2.f8eff138.chunk.js"
+  },
+  {
+    "revision": "7baa2d7b08d7c74c1c7b",
+    "url": "/todolist/static/js/main.da523fc1.chunk.js"
+  },
+  {
+    "revision": "0d82162ed29ebd5aa62b",
+    "url": "/todolist/static/js/runtime-main.8a4d4dc4.js"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/todolist/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/todolist/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/todolist/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/todolist/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/todolist/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/todolist/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/todolist/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/todolist/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/todolist/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/todolist/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/todolist/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/todolist/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/todolist/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/todolist/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/todolist/static/media/fa-solid-900.d824df7e.woff2"
+  }
+]);
